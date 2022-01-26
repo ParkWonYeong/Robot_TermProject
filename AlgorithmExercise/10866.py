28d470904568f5ce988d34e59adbfda03eefab3e
WonYeong Park <parkwy1010@gmail.com>
2022-01-26 23:13:03 +0900

    22-01-26

--- AlgorithmExercise/10866.py
+++ AlgorithmExercise/10866.py
@@ -6,60 +6,60 @@ input = sys.stdin.readline
 
 # push_front X 명령어
 def push_front(x): 
-    deque.appendleft(0, command[1])
+    q.appendleft(command[1])
     # 또는
     # tmp = [x]
     # tmp.extend(deq)
-    # deque = tmp
-    # return deque
+    # q = tmp
+    # return q
 
 # push_back x 명령어
 def push_back(x):
-    deque.append(0, command[1])
+    q.append(command[1])
 
 # pop_front 명령어
 def pop_front():
-    if (not deque):
+    if (not q):
         return -1   # 스택에 들어있는 정수가 없는 경우 -1 출력
     else:
-        return deque.pop()    # 스택에서 가장 위에 있는 정수를 빼고 그 수 출력
+        return q.popleft()    # 스택에서 가장 위에 있는 정수를 빼고 그 수 출력
 
 # pop_back 명령어
 def pop_back():
-    if (not deque):
+    if (not q):
         return -1
     else:
-        return deque.pop_back()
+        return q.pop()
 
 # size 명령어
 def size():
-    return len(deque) # 덱에 들어있는 정수의 개수 출력
+    return len(q) # 덱에 들어있는 정수의 개수 출력
 
 # empty 명령어
 def empty():
-    if deque:
+    if q:
         return 0    # 덱이 비어있지 않으면 0 출력
     else:
         return 1    # 덱이 비어있으면 1 출력
 
 # front 명령어
 def front():
-    if deque:
-        return deque[len(deque)-1]  # 덱의 가장 앞에 있는 정수
+    if q:
+        return q[0]  # 덱의 가장 앞에 있는 정수
     else:
         return -1   # 덱에 들어있는 정수가 없으면 -1 출력
 
 # back 명령어
 def back():
-    if deque:
-        return deque[0] # 큐의 가장 뒤에 있는 정수
+    if q:
+        return q[-1] # 큐의 가장 뒤에 있는 정수
     else:
         return -1   # 큐에 들어있는 정수가 없으면 -1 출력
 
 
 N=int(input())  # 주어지는 명령 수 N
 
-deque=[]
+q=deque([])
 for _ in range(N):
     command = input().split()
     order = command[0]
@@ -80,3 +80,20 @@ for _ in range(N):
         print(front())
     else:
         print(back())
+
+# 참고한 코드(출처 https://e-you.tistory.com/293)
+# from collections import deque
+# import sys
+# input=sys.stdin.readline
+# n = int(input())
+# q=deque([])
+# for _ in range(n):
+#     arr = input().split()
+#     if arr[0]=="push_front": q.appendleft(int(arr[1]))
+#     elif arr[0]=="push_back": q.append(int(arr[1]))
+#     elif arr[0]=="pop_front": print(q.popleft()) if q else print(-1)
+#     elif arr[0]=="pop_back": print(q.pop()) if q else print(-1)
+#     elif arr[0]=="size": print(len(q))
+#     elif arr[0]=="empty": print(0) if q else print(1)
+#     elif arr[0]=="front" : print(q[0]) if q else print(-1)
+#     elif arr[0]=="back" : print(q[-1]) if q else print(-1)
